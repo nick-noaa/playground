9e62895d46313650a96d11843e4ff77981e37530
Nick <108428878+nick-noaa@users.noreply.github.com>
2023-02-23 20:32:25 +0000

    added React.js

---
+++ src/components/React.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react';
+
+function React() {
+
+  const [name, setName] = useState('Team')
+
+  return <div>Hello {name} This is a React Component!</div>;
+}
+
+export default React;
