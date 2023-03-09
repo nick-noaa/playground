a72ae9457cd2f50c3e2e5dec53b23737f84b094c
Nick <108428878+nick-noaa@users.noreply.github.com>
2023-03-09 22:57:24 +0000

    playing around with postcss-preset-env and open-props

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,44 +1,43 @@
+const primaryNav = document.querySelector('ul');
+const navToggle = document.getElementById('menu-toggle');
 
-  const primaryNav = document.querySelector('ul') as HTMLElement;
-  const navToggle = document.getElementById('menu-toggle') as HTMLElement;
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible');
 
-  navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible');
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
 
-    if (visibility === 'false') {
-      primaryNav.setAttribute('data-visible', true);
-      navToggle.setAttribute('aria-expanded', true);
-    } else {
-      primaryNav.setAttribute('data-visible', false);
-      navToggle.setAttribute('aria-expanded', false);
-    }
+  // Toggle class "opened". Set also aria-expanded to true or false.
+  if (-1 !== navToggle.className.indexOf('opened')) {
+    navToggle.className = navToggle.className.replace(' opened', '');
+    navToggle.setAttribute('aria-expanded', 'false');
+  } else {
+    navToggle.className += ' opened';
+    navToggle.setAttribute('aria-expanded', 'true');
+  }
+});
 
-    // Toggle class "opened". Set also aria-expanded to true or false.
-    if (-1 !== navToggle.className.indexOf('opened')) {
-      navToggle.className = navToggle.className.replace(' opened', '');
-      navToggle.setAttribute('aria-expanded', 'false');
-    } else {
-      navToggle.className += ' opened';
-      navToggle.setAttribute('aria-expanded', 'true');
-    }
-  });
+const setGlowEffectRx = () => {
+  const glowEffects = document.querySelectorAll('.glow');
 
-  const setGlowEffectRx = () => {
-    const glowEffects = document.querySelectorAll('.glow');
+  glowEffects.forEach(glowEffect => {
+    const glowLines = glowEffect.querySelectorAll('rect');
+    const rx = getComputedStyle(glowEffect, '::after').borderRadius;
 
-    glowEffects.forEach(glowEffect => {
-      const glowLines = glowEffect.querySelectorAll('rect');
-      const rx = getComputedStyle(glowEffect, '::after').borderRadius;
+    //NOTE setup for any added JS micro interactions.
+    // glowEffect.addEventListener('click', () => {
 
-      //NOTE setup for any added JS micro interactions.
-      // glowEffect.addEventListener('click', () => {
+    // });
 
-      // });
-
-      glowLines.forEach(line => {
-        line.setAttribute('rx', rx);
-      });
+    glowLines.forEach(line => {
+      line.setAttribute('rx', rx);
     });
-  };
+  });
+};
 
-  setGlowEffectRx();
+setGlowEffectRx();
