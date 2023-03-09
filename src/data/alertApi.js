3da5bb76b6325d98540d2f7fe053cb11b3d46aa6
Nick <108428878+nick-noaa@users.noreply.github.com>
2023-03-09 16:50:11 +0000

    just some clean up and messing around with feats

---
+++ src/data/alertApi.js
@@ -0,0 +1,169 @@
+const alertApi = [
+  {
+    nid: [
+      {
+        value: 3416,
+      },
+    ],
+    uuid: [
+      {
+        value: '1d656c6e-5a6d-4d27-8ba3-c54daa0fc9d2',
+      },
+    ],
+    vid: [
+      {
+        value: 40721,
+      },
+    ],
+    langcode: [
+      {
+        value: 'en',
+      },
+    ],
+    type: [
+      {
+        target_id: 'ncei_alert',
+        target_type: 'node_type',
+        target_uuid: '3ec5bdcc-9368-4f83-b71f-6991c942a8ed',
+      },
+    ],
+    revision_timestamp: [
+      {
+        value: '2023-02-23T20:00:41+00:00',
+        format: 'Y-m-d\\TH:i:sP',
+      },
+    ],
+    revision_uid: [
+      {
+        target_id: 993,
+        target_type: 'user',
+        target_uuid: '939fde66-3376-4c1b-9185-caa490238c7a',
+        url: '/editors/993',
+      },
+    ],
+    revision_log: [],
+    status: [
+      {
+        value: true,
+      },
+    ],
+    uid: [
+      {
+        target_id: 993,
+        target_type: 'user',
+        target_uuid: '939fde66-3376-4c1b-9185-caa490238c7a',
+        url: '/editors/993',
+      },
+    ],
+    title: [
+      {
+        value: 'NEWIS Retirement',
+      },
+    ],
+    created: [
+      {
+        value: '2023-02-23T19:58:14+00:00',
+        format: 'Y-m-d\\TH:i:sP',
+      },
+    ],
+    changed: [
+      {
+        value: '2023-02-23T20:00:41+00:00',
+        format: 'Y-m-d\\TH:i:sP',
+      },
+    ],
+    promote: [
+      {
+        value: false,
+      },
+    ],
+    sticky: [
+      {
+        value: false,
+      },
+    ],
+    default_langcode: [
+      {
+        value: true,
+      },
+    ],
+    revision_translation_affected: [
+      {
+        value: true,
+      },
+    ],
+    moderation_state: [],
+    ds_switch: [],
+    metatag: {
+      value: {
+        canonical_url: 'https://www.ncei.noaa.gov/alerts/api',
+        shortlink: 'https://www.ncei.noaa.gov/alerts/api',
+        generator: 'Drupal 8 (http://drupal.org)',
+        title:
+          'NEWIS Retirement | National Centers for Environmental Information (NCEI)',
+        description:
+          'NCEI will be retiring the NOAA Extreme Weather Information Sheets (NEWIS) on May 1, 2023.',
+        keywords:
+          'climate,data,observations,weather,coasts,oceans,geophysics,satellites',
+        shortcut_icon:
+          'https://www.ncei.noaa.gov/themes/custom/ncei/favicon.ico',
+        og_site_name: 'National Centers for Environmental Information (NCEI)',
+        og_type: 'article',
+        og_url: 'https://www.ncei.noaa.gov/alerts/api',
+        og_title: 'NEWIS Retirement',
+        og_description:
+          'NCEI will be retiring the NOAA Extreme Weather Information Sheets (NEWIS) on May 1, 2023.',
+        og_updated_time: '2023-02-23T15:00:41-0500',
+        article_published_time: '2023-02-23T14:58:14-0500',
+        article_modified_time: '2023-02-23T15:00:41-0500',
+        twitter_cards_type: 'summary_large_image',
+        twitter_cards_description:
+          'NCEI will be retiring the NOAA Extreme Weather Information Sheets (NEWIS) on May 1, 2023.',
+        twitter_cards_title: 'NEWIS Retirement',
+        twitter_cards_page_url: 'https://www.ncei.noaa.gov/alerts/api',
+      },
+    },
+    path: [
+      {
+        alias: null,
+        pid: null,
+        langcode: 'en',
+      },
+    ],
+    body: [
+      {
+        value:
+          '<p>NCEI will be retiring the NOAA Extreme Weather Information Sheets (NEWIS) on May 1, 2023.</p>\r\n',
+        format: 'basic_html',
+        processed:
+          '<p>NCEI will be retiring the NOAA Extreme Weather Information Sheets (NEWIS) on May 1, 2023.</p>',
+        summary: '',
+      },
+    ],
+    field_alert_type: [
+      {
+        value: 'severity-high',
+      },
+    ],
+    field_end_date: [
+      {
+        value: '2023-05-01T17:00:00-04:00',
+      },
+    ],
+    field_metadata_id: [],
+    field_metadata_id_2: [],
+    field_node_reference: [
+      {
+        target_id: 1364,
+        target_type: 'node',
+        target_uuid: '0cb61242-08d7-4bb0-927e-741a3cc621f5',
+        url: '/resources/newis',
+      },
+    ],
+    field_start_date: [
+      {
+        value: '2023-02-23T15:01:00-05:00',
+      },
+    ],
+  },
+];
