52d657e8e2a9df0f11e7c78144925dfeaccb717c
Nick <108428878+nick-noaa@users.noreply.github.com>
2023-03-11 01:07:35 +0000

    trying some out different things with postcss and astro.

---
+++ src/data/navData.js
@@ -0,0 +1,28 @@
+const navData = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Products',
+    path: '/',
+  },
+  {
+    name: 'Services',
+    path: '/',
+  },
+  {
+    name: 'News',
+    path: '/',
+  },
+  {
+    name: 'About',
+    path: '/about',
+  },
+  {
+    name: 'Contact',
+    path: '/',
+  },
+];
+
+export default navData;
