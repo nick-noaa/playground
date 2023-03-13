06ff7bffe548c9a24de2b0cf1928e47576f5b1f9
Nick <108428878+nick-noaa@users.noreply.github.com>
2023-03-13 22:24:06 +0000

    Pending changes exported from your codespace

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-jit-props')({
-      files: ['./src/styles/abstracts/**'],
+      files: ['./src/styles/*/**'],
     }),
     require('postcss-preset-env')({ stage: 0 }),
   ],
