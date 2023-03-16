0222467439f2de3ad060f2e1e7bd44428f07e7f0
Nick <nick.simpson@noaa.gov>
2023-03-16 17:49:58 +0000

    edditing more on alerts component

---
+++ src/js/dateFunctions.js
@@ -0,0 +1,8 @@
+const changeDateFormat = date => {
+  return new Date(date).toLocaleDateString('en-us', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+};
